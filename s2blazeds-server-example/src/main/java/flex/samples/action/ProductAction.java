c931e8b92812d473d30373742b692910474cafe7
Yasuo Higa <higayasuo@gmail.com>
2008-03-12 05:56:55 +0000

    updatemain.mxmlとthumbnailmain.mxmlを追加。
    git-svn-id: https://www.seasar.org/svn/s2blazeds/trunk@20 5aa78d1d-1886-4248-95ba-84addde6a225

--- s2blazeds-server-example/src/main/java/flex/samples/action/ProductAction.java
+++ s2blazeds-server-example/src/main/java/flex/samples/action/ProductAction.java
@@ -7,19 +7,52 @@ import org.seasar.struts.annotation.Execute;
 
 import flex.samples.entity.Product;
 
+/**
+ * プロダクトを扱うアクションです。
+ * 
+ * @author higa
+ * 
+ */
 public class ProductAction {
 
+	/**
+	 * JDBCマネージャです。
+	 */
 	public JdbcManager jdbcManager;
 
+	/**
+	 * プロダクトのリストです。
+	 */
 	public List<Product> items;
 
+	/**
+	 * インデックス用の実行メソッドです。
+	 * 
+	 * @return 遷移先
+	 */
 	@Execute(validator = false)
 	public String index() {
 		items = getProducts();
 		return "products.jsp";
 	}
 
+	/**
+	 * プロダクトのリストを返します。
+	 * 
+	 * @return プロダクトのリスト
+	 */
 	public List<Product> getProducts() {
 		return jdbcManager.from(Product.class).getResultList();
+	}
+
+	/**
+	 * プロダクトを更新します。
+	 * 
+	 * @param product
+	 *            プロダクト
+	 * @return 更新した行数
+	 */
+	public int update(Product product) {
+		return jdbcManager.update(product).execute();
 	}
 }
